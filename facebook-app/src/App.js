29a0c405b2b5fffa461e434a609d70ec5c71cda1
Ivan Rosario <ivanrosario@Administrators-MacBook-Pro-2.local>
2017-10-24 21:54:15 -0400

    tidy js

--- facebook-app/src/App.js
+++ facebook-app/src/App.js
@@ -2,26 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
+//use constructor when messing with state and binding functions 
   constructor(){
     super();
-    this.getUserInput = this.getUserInput.bind(this);
-    this.addUserComment = this.addUserComment.bind(this);
 
     this.state = {
       userInput: '',
       commentList: [],
     }
-  };
 
-  getUserInput(event) {
+  };
+//all binding done on the onClicks
+  getUserInput  (event) {
     let comment = event.target.value;
     this.setState({
       userInput: comment,
     });
-
   };
-
+//know where to add the reply with index
   getUserReply(event, i){
     let replyInput = event.target.value
     let state = Object.assign({}, this.state);
@@ -30,37 +28,42 @@ class App extends Component {
       state,
     })
   }
+  //makes a new object for the repy  a
+  //push it into replies array of comment
   addReplyComment(i){
     let state = Object.assign({}, this.state);
-    const newReply = 
+    const newReply =
           {
             reply: state.commentList[i].replyInput,
             replyLike:0,
-            replydislike:0, 
+            replydislike:0,
          }
-        
+
     state.commentList[i].replies.push(newReply);
     this.setState({
       state,
     });
   }
-  addUserComment() {
+  //makes a new obj for the array of comments
+  addUserComment = () => {
+    //makes a copy of the array
     let getCommentsArray = this.state.commentList.slice();
-    const newComment = 
+    //new comment
+    const newComment =
         {
           comment: this.state.userInput,
           likes: 0,
           dislikes: 0,
           replyInput: '',
           replies: [],
-     }
-
+        }
+// pushing a new comment into to the list
     getCommentsArray.push(newComment);
     this.setState({
       commentList: getCommentsArray,
     });
   };
-
+//remove a comment by index
   handleRemoveClick(i){
     let commentIndex = i;
     let commentList = this.state.commentList;
@@ -69,15 +72,15 @@ class App extends Component {
       commentList: commentList,
     })
   }
-
+//reaction target value
   handleResponseClick(reaction, i) {
     let state = Object.assign({}, this.state);
     if (reaction === "like") {
-      // set state to comment with new like
-        state.commentList[i].likes++;
+
+      state.commentList[i].likes++;
     } else if (reaction === "dislike") {
-      // set state to comment with new dislike
-        state.commentList[i].dislikes++;
+
+      state.commentList[i].dislikes++;
     }
     this.setState({
       state
@@ -87,10 +90,9 @@ class App extends Component {
   replyHandle(add, i, childindex){
     let state = Object.assign({}, this.state);
      if (add === "good") {
-      // set state to comment with new like
+      // I shows which comment we are on || childindex shows which reply we are on
           state.commentList[i].replies[childindex].replyLike++;
     } else if (add === "bad") {
-      // set state to comment with new dislike
           state.commentList[i].replies[childindex].replydislike++;
     }
     this.setState({
@@ -98,18 +100,26 @@ class App extends Component {
     })
   }
 
+
+
   render() {
+    //map function:
+    // calls a defined callback function on each element of an array,
+    // and returns an array that contains the results.
+
     const commentList = this.state.commentList.map(function(commentObj, i){
       const replytocomment = this.state.commentList[i].replies.map(function(replyobj, childindex){
+        //childIndex
         return(
           <div key={childindex}>
             <p >{replyobj.reply}</p>
             <button value="good" onClick={(add)=>{this.replyHandle("good", i,  childindex)}}>{this.state.commentList[i].replies[childindex].replyLike} Like</button>
-            <button value="bad" onClick={(add)=>{this.replyHandle("bad", i,  childindex)}}>{this.state.commentList[i].replies[childindex].replydislike} Like</button>
+            <button value="bad" onClick={(add)=>{this.replyHandle("bad", i,  childindex)}}>{this.state.commentList[i].replies[childindex].replydislike} Disike</button>
           </div>
         )
       }, this);
       return (
+        //top comment
         <li key={i}>{commentObj.comment}
           <button value="like" onClick={(reaction)=>{this.handleResponseClick("like", i)}}>{this.state.commentList[i].likes} Like</button>
           <button value="dislike" onClick={(reaction)=>{this.handleResponseClick("dislike", i)}}> {this.state.commentList[i].dislikes} Dislike</button>
@@ -125,7 +135,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>RedTalk</h1>
-        <input type="text" value={this.state.userInput} onChange={this.getUserInput} />
+        <input type="text" value={this.state.userInput} onChange={(event) => {this.getUserInput(event)}} />
         <button onClick={this.addUserComment}>Submit</button>
         <ul>
         {commentList}
--- facebook-app/src/Inputbox.js
+++ facebook-app/src/Inputbox.js
@@ -3,18 +3,17 @@ import PropTypes from 'prop-types';
 
 //the input
 function InputBox(props) {
-
-
-    return (
-      <div >
-        <div className="Guess">
-          <h2>Input Answer</h2>
-          <input type="text"  name="Guess" onChange={props.UserInput}  />
-          <button id="addBtn" onClick={props.handlComment}>Add</button>
-        </div>
+  return (
+    <div >
+      <div className="Guess">
+        <h2>Input Answer</h2>
+        <input type="text"  name="Guess" onChange={props.UserInput}  />
+        <button id="addBtn" onClick={props.handlComment}>Add</button>
       </div>
+    </div>
     )
 } 
+  
 InputBox.propTypes = {
   handlComment: PropTypes.func,
   UserInput: PropTypes.func,
